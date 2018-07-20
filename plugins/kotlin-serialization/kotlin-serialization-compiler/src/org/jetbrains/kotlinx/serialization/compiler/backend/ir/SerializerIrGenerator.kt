2cca4ecc0f7d4c83b0d65f3267f98cc79ac4eb9d
Leonid Startsev <sandwwraith@gmail.com>
2018-07-20 19:10:19 +0300

    Updates for new API

--- plugins/kotlin-serialization/kotlin-serialization-compiler/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/SerializerIrGenerator.kt
+++ plugins/kotlin-serialization/kotlin-serialization-compiler/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/SerializerIrGenerator.kt
@@ -49,7 +49,7 @@ class SerializerIrGenerator(val irClass: IrClass, override val compilerContext: 
     override val translator: TypeTranslator = TypeTranslator(compilerContext.symbolTable)
 
     override fun generateSerialDesc() {
-        val desc: PropertyDescriptor = serialDescPropertyDescriptor ?: return
+        val desc: PropertyDescriptor = generatedSerialDescPropertyDescriptor ?: return
         val serialDescImplClass = serializerDescriptor
             .getClassFromInternalSerializationPackage(SERIAL_DESCRIPTOR_CLASS_IMPL)
         val serialDescImplConstructor = serialDescImplClass
@@ -103,7 +103,7 @@ class SerializerIrGenerator(val irClass: IrClass, override val compilerContext: 
                     +irSetField(
                         generateReceiverExpressionForFieldAccess(
                             thisAsReceiverParameter.symbol,
-                            serialDescPropertyDescriptor
+                            generatedSerialDescPropertyDescriptor
                         ),
                         prop.backingField!!,
                         irGet(localDesc)
@@ -174,7 +174,7 @@ class SerializerIrGenerator(val irClass: IrClass, override val compilerContext: 
 
         val kOutputClass = serializerDescriptor.getClassFromSerializationPackage(STRUCTURE_ENCODER_CLASS)
 
-        val descriptorGetterSymbol = compilerContext.symbolTable.referenceFunction(serialDescPropertyDescriptor?.getter!!)
+        val descriptorGetterSymbol = compilerContext.symbolTable.referenceFunction(anySerialDescProperty?.getter!!)
 
         val localSerialDesc = irTemporary(irGet(descriptorGetterSymbol.owner.returnType, irThis(), descriptorGetterSymbol), "desc")
 
@@ -262,7 +262,7 @@ class SerializerIrGenerator(val irClass: IrClass, override val compilerContext: 
         fun IrVariable.get() = irGet(this)
 
         val inputClass = serializerDescriptor.getClassFromSerializationPackage(STRUCTURE_DECODER_CLASS)
-        val descriptorGetterSymbol = compilerContext.symbolTable.referenceFunction(serialDescPropertyDescriptor?.getter!!)
+        val descriptorGetterSymbol = compilerContext.symbolTable.referenceFunction(anySerialDescProperty?.getter!!)
         val localSerialDesc = irTemporary(irGet(descriptorGetterSymbol.owner.returnType, irThis(), descriptorGetterSymbol), "desc")
 
         // workaround due to unavailability of labels (KT-25386)
