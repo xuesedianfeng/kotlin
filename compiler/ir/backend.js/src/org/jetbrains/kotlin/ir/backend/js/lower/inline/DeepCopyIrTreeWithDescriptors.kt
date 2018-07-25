0f57b1005f1261dbb9f1ca7020440c7940aeee1c
Roman Artemev <Roman.Artemev@jetbrains.com>
2018-07-25 23:03:09 +0300

    Fake wrapped descriptors first working draft get rid of descriptors in Bridge and Enum lowerings Replace property accessors function type with IrSimpleFunction because they couldn't be constructors get rid of descriptors in Callable reference lowering refactored descriptor factory and inner class lowering Add isReified property to IrTypeParameter declaration keep getting rid of descriptors Get rid of descriptors in Shared Variable Manager LocalDeclarationLowering also uses no descriptors Fix psi2ir Fix nested classes names Fix outer reference in inner classes Fix name generator get rid of descriptors in coroutines - something is working Fix name generator Fix unbound symbols in JVM BE Rename DeclarationFactory members Partially reimplement KotlinType based util using IrType

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/inline/DeepCopyIrTreeWithDescriptors.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/inline/DeepCopyIrTreeWithDescriptors.kt
@@ -479,7 +479,7 @@ internal class DeepCopyIrTreeWithDescriptors(val targetDescriptor: FunctionDescr
             return IrCallImpl(
                 startOffset    = expression.startOffset,
                 endOffset      = expression.endOffset,
-                type           = newDescriptor.returnType?.toIrType()!!,
+                type           = newDescriptor.returnType?.toIrType(context.symbolTable)!!,
                 descriptor     = newDescriptor,
                 typeArgumentsCount = expression.typeArgumentsCount,
                 origin         = expression.origin,
@@ -611,7 +611,7 @@ internal class DeepCopyIrTreeWithDescriptors(val targetDescriptor: FunctionDescr
 
     //-------------------------------------------------------------------------//
 
-    private fun substituteType(oldType: IrType?): IrType? = substituteType(oldType?.toKotlinType())?.toIrType()
+    private fun substituteType(oldType: IrType?): IrType? = substituteType(oldType?.toKotlinType())?.toIrType(context.symbolTable)
 
     private fun substituteType(oldType: KotlinType?): KotlinType? {
         if (typeSubstitutor == null) return oldType
