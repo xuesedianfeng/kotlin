9b915d4ad83c6715a85addebe49b0b93a9f9dd5f
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2018-08-20 22:02:03 +0300

    Minor. Document functions

--- libraries/stdlib/coroutines/jvm/src/kotlin/coroutines/jvm/internal/DebugMetadata.kt
+++ libraries/stdlib/coroutines/jvm/src/kotlin/coroutines/jvm/internal/DebugMetadata.kt
@@ -23,12 +23,22 @@ internal annotation class DebugMetadata(
     val debugIndexToLabel: IntArray
 )
 
+/**
+ * Returns file name and line number of current coroutine's suspension point. The coroutine can be either running coroutine, that calls
+ * the function on its continuation and obtaining the information about current file and line number, or, more likely, the function is
+ * called to produce debug-friendly [toString] output of suspended coroutine (i.e. where it has suspended).
+ * Additionally, the function is used to obtain [StackTraceElement]s from continuations' stack in order to create more natural stack traces.
+ */
 @SinceKotlin("1.3")
 public fun getSourceFileAndLineNumber(c: Continuation<*>): Pair<String, Int> {
     val debugMetadata = c.getDebugMetadataAnnotation()
     return debugMetadata.runtimeSourceFiles.zip(debugMetadata.runtimeLineNumbers.asList())[c.getLabel() ?: return "" to -1]
 }
 
+/**
+ * The same as [getSourceFileAndLineNumber], but returns [String] instead of [Pair] to simplify data extraction on debugger's end.
+ * The return string has the following pattern: "$fileName:$lineNumber".
+ */
 @SinceKotlin("1.3")
 public fun getSourceFileAndLineNumberForDebugger(c: Continuation<*>): String {
     val pair = getSourceFileAndLineNumber(c)
@@ -46,6 +56,15 @@ private fun Continuation<*>.getLabel(): Int? {
     return field.get(this) as Int - 1
 }
 
+/**
+ * Returns an array of spilled variable names and continuation's field names where the variable has been spilled.
+ * The structure is the following:
+ * - field names take 2*k'th indices
+ * - corresponding variable names take (2*k + 1)'th indices.
+ *
+ * Like [getSourceFileAndLineNumberForDebugger], the function is for debugger to use, thus it returns simplest data type possible.
+ * This function should only be called on suspended coroutines to get accurate mapping.
+ */
 @SinceKotlin("1.3")
 public fun getVariableToSpilledMapping(c: Continuation<*>): Array<String> {
     val debugMetadata = c.getDebugMetadataAnnotation()
