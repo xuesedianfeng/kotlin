f2e970be1f3ace1f4049fc2796c863ead087c1ee
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2018-09-10 17:56:13 +0300

    Get rid of USE_VALUE_OF flag

--- libraries/stdlib/coroutines/jvm/src/kotlin/coroutines/jvm/internal/boxing.kt
+++ libraries/stdlib/coroutines/jvm/src/kotlin/coroutines/jvm/internal/boxing.kt
@@ -5,26 +5,14 @@
 
 @file:JvmName("Boxing")
 
-@file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN", "ConstantConditionIf")
+@file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
 
 package kotlin.coroutines.jvm.internal
 
 /*
- * A flag to switch between boxing strategies for coroutines. We use two different strategies for boxing primitives in coroutine code:
- * 1) Use standard [valueOf] method.
- * 2) Allocate primitive wrapper object every time.
+ * Box primitive to Java wrapper class by allocating the wrapper object.
  *
- * The latter is default.
- *
- * This is a static final boolean field for HotSpot to optimize it away.
- */
-private val USE_VALUE_OF = false
-
-/*
- * Box primitive to Java wrapper class, using either standard [valueOf] method or by allocating the wrapper object, depending on value of
- * [USE_VALUE_OF] flag.
- *
- * The latter allows HotSpot JIT to eliminate allocations completely in coroutines code with primitives.
+ * This allows HotSpot JIT to eliminate allocations completely in coroutines code with primitives.
  */
 
 @SinceKotlin("1.3")
@@ -34,19 +22,13 @@ internal fun boxBoolean(primitive: Boolean): java.lang.Boolean = java.lang.Boole
 internal fun boxByte(primitive: Byte): java.lang.Byte = java.lang.Byte.valueOf(primitive) as java.lang.Byte
 
 @SinceKotlin("1.3")
-internal fun boxShort(primitive: Short): java.lang.Short =
-    if (USE_VALUE_OF) java.lang.Short.valueOf(primitive) as java.lang.Short
-    else java.lang.Short(primitive)
+internal fun boxShort(primitive: Short): java.lang.Short = java.lang.Short(primitive)
 
 @SinceKotlin("1.3")
-internal fun boxInt(primitive: Int): java.lang.Integer =
-    if (USE_VALUE_OF) java.lang.Integer.valueOf(primitive) as java.lang.Integer
-    else java.lang.Integer(primitive)
+internal fun boxInt(primitive: Int): java.lang.Integer = java.lang.Integer(primitive)
 
 @SinceKotlin("1.3")
-internal fun boxLong(primitive: Long): java.lang.Long =
-    if (USE_VALUE_OF) java.lang.Long.valueOf(primitive) as java.lang.Long
-    else java.lang.Long(primitive)
+internal fun boxLong(primitive: Long): java.lang.Long = java.lang.Long(primitive)
 
 @SinceKotlin("1.3")
 internal fun boxFloat(primitive: Float): java.lang.Float = java.lang.Float(primitive)
@@ -55,6 +37,4 @@ internal fun boxFloat(primitive: Float): java.lang.Float = java.lang.Float(primi
 internal fun boxDouble(primitive: Double): java.lang.Double = java.lang.Double(primitive)
 
 @SinceKotlin("1.3")
-internal fun boxChar(primitive: Char): java.lang.Character =
-    if (USE_VALUE_OF) java.lang.Character.valueOf(primitive) as java.lang.Character
-    else java.lang.Character(primitive)
+internal fun boxChar(primitive: Char): java.lang.Character = java.lang.Character(primitive)
